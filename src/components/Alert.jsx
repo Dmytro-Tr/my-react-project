989d9e7c9310862279a69a18bce79a19e0ec4471
Dmtro_Tr <Di4gamegem@gmail.com>
2024-12-04 13:56:43 +0200

    Modul-1 Lesson-2

---
+++ src/components/Alert.jsx
@@ -0,0 +1,15 @@
+import "./Alert.css";
+
+const Alert = ({ variant, outlined, elevated, children }) => {
+  const classNAme = ["alert", variant];
+
+  if (outlined) {
+    classNAme.push("is-outlined");
+  }
+
+  if (elevated) {
+    classNAme.push("is-elevated");
+  }
+
+  return <p className={classNAme.join(" ")}>{children}</p>;
+};
